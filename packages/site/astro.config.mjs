42dc143e30485c0267a44d7288d3d08774a1d030
RavonBarksdale <121208995+RavonBarksdale@users.noreply.github.com>
2025-06-07 16:45:43 -0700

    feat(site): enable wiki-link plugin

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import wikiLinkPlugin from '@portaljs/remark-wiki-link';
 export default defineConfig({
   integrations: [mdx()],
   markdown: {
-    remarkPlugins: [],
+    remarkPlugins: [
+      [wikiLinkPlugin, {
+        hrefTemplate: (permalink) => `/notes/${permalink}`
+      }]
+    ],
   },
 });
